b921713dd903230b659cc32f28f716fd260c2b4e
norio.akagi <norio.akagi@playnext.com>
2015-08-27 01:39:17 -0700

    Modify line breaks

--- Binary_Tree_Level_Order_Traversal/src.py
+++ Binary_Tree_Level_Order_Traversal/src.py
@@ -16,18 +16,20 @@ class Solution(object):
 
         res = []
         res.append([root.val])
-
-
         rootQueue = [root]
+
         while True:
             childrenQueue = []
+
             while rootQueue:
                 parent = rootQueue[0]
+
                 if parent.left:
                     childrenQueue.append(parent.left)
                 if parent.right:
                     childrenQueue.append(parent.right)
                 rootQueue.pop(0)
+                
             if childrenQueue:
                 res.append([item.val for item in childrenQueue])
                 rootQueue = childrenQueue
